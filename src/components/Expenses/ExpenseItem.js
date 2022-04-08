5793fc5c8095738539d89b9b8413654c892fd06e
Andrew Marshall <andrewleonmarshall@gmail.com>
2022-04-07 23:21:25 -0400

    Added button with useState and react hook

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,27 @@
+import React, { useState } from "react";
+
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  // [prevVal, newVal]
+  const [title, setTitle] = useState(props.title); // react hook
+  // All hooks start with 'use'
+  // Must be called inside react component (with 1 exception)
+
+  const clickHandler = () => {
+    setTitle("Updated!"); // Doesn't immediately change. Schedules change to be done
+  };
+
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
